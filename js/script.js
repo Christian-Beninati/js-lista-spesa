7b8b435eec3ac93693c53ce8db39125ed2fefdea
Christian-Beninati <christianbeninati.info@gmail.com>
2023-06-05 14:12:23 +0100

    cycle -for -while

--- js/script.js
+++ js/script.js
@@ -1,1 +1,55 @@
-console.log('JS OK')
+// console.log('JS OK')
+
+// CICLO FOR ••••••••••••••••••••••••••••••••••••••••••••••••••••
+
+// Creo un Array con lista della spesa 
+// const listItems = ['pane', 'latte', 'acqua', 'pasta']
+// console.log(listItems);
+
+// Recupero elemento dal DOM
+// const shoppingList = document.getElementById ('shopping-list')
+
+// Creo una variabile per costruire la lista
+// let list = '';
+
+// Itero attraverso gli elementi dell'array
+// for (let i = 0; i < listItems.length; i++) {
+    // Creo gli elementi della lista 
+//     list += `<li>${listItems[i]}</li>`
+// }
+
+// Inserisco il risultato nella pagina HTML
+// shoppingList.innerHTML = list;
+
+
+
+
+
+
+
+// CICLO WHILE ••••••••••••••••••••••••••••••••••••••••••••••••••••
+
+// Creo un Array con lista della spesa 
+const listItems = ['pane', 'latte', 'acqua', 'pasta']
+console.log(listItems);
+
+// Recupero elemento dal DOM
+const shoppingList = document.getElementById ('shopping-list')
+
+// Creo una variabile per costruire la lista
+let list = '';
+
+// Inizializzo l'indice a 0
+let i = 0;
+
+// Itero finché l'indice è inferiore alla lunghezza dell'array
+while (i < listItems.length) {
+    // Creo gli elementi della lista 
+    list += `<li>${listItems[i]}</li>`
+
+    // Incremento l'indice
+    i++;
+}
+
+// Inserisco il risultato nella pagina HTML
+shoppingList.innerHTML = list;
---
+++ steps.md
@@ -0,0 +1,37 @@
+## Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while.
+
+Consigli:
+
+- Facciamo un primo giro col for, in modo da sentirci confident sul risultato della stampa, poi trasformiamo il for in un while
+- Ricordiamoci di inizializzare la variabile di contatore prima dell'inizio del ciclo while
+- Ricordiamoci di incrementare il contatore all'interno ciclo while per evitare di cascare in un loop infinito.
+
+<br>
+<br>
+
+## Ciclo FOR
+
+- Inizio
+- Creo un Array con la lista della spesa
+- Recupero l'elemento dal DOM
+- Creo una variabile per costruire la lista
+- Itero attraverso gli elementi dell'Array
+- Creo gli elementi della lista
+- Inserisco il risulttato nella pagina HTML
+- Fine
+
+<br>
+<br>
+
+## Ciclo WHILE
+
+- Inizio
+- Creo un Array con la lista della spesa
+- Recupero l'elemento dal DOM
+- Creo una variabile per costruire la lista
+- Inizializzo l'indice a 0
+- Itero finché l'indice è inferiore alla lunghezza dell'Array
+- Creo gli elementi della lista
+- Incremento l'indice
+- Inserisco il risulttato nella pagina HTML
+- Fine
